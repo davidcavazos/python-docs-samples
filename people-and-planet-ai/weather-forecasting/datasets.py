0f971ce1ef4200e18fce1cc5016efad9701a599a
David Cavazos <dcavazos@google.com>
2022-09-12 18:17:28 -0700

    optimize number of requests to ee

--- people-and-planet-ai/weather-forecasting/datasets.py
+++ people-and-planet-ai/weather-forecasting/datasets.py
@@ -15,29 +15,17 @@
 from datetime import datetime, timedelta
 from typing import Dict, Iterable, List, Optional, NamedTuple, Tuple
 import io
-import requests
 import logging
 import random
+import requests
+import uuid
 
 import ee
 from google.api_core import retry, exceptions
 import google.auth
 import numpy as np
+from numpy.lib.recfunctions import structured_to_unstructured
 import pandas as pd
-
-INPUTS = {
-    "NOAA/GOES/16/MCMIPF": ["CMI_.*"],
-    "NASA/GPM_L3/IMERG_V06": ["precipitationCal"],
-}
-INPUT_HOUR_DELTAS = [-2, -1, 0]
-
-LABELS = {
-    "NASA/GPM_L3/IMERG_V06": ["precipitationCal"],
-}
-LABEL_HOUR_DELTAS = [1, 6]
-
-SCALE = 10000
-IMAGE_TIME_WINDOW = timedelta(days=1)
 
 
 class Bounds(NamedTuple):
@@ -48,7 +36,6 @@ class Bounds(NamedTuple):
 
 
 class Point(NamedTuple):
-    date: datetime
     lat: float
     lon: float
 
@@ -56,6 +43,22 @@ class Point(NamedTuple):
 class Example(NamedTuple):
     inputs: np.ndarray
     labels: np.ndarray
+
+
+INPUTS = {
+    "NOAA/GOES/16/MCMIPF": [f"CMI_C{i:02}" for i in range(1, 16 + 1)],
+    "NASA/GPM_L3/IMERG_V06": ["precipitationCal"],
+}
+
+LABELS = {
+    "NASA/GPM_L3/IMERG_V06": ["precipitationCal"],
+}
+
+SCALE = 10000
+WINDOW = timedelta(hours=1)
+
+START_DATE = datetime(2017, 7, 10)
+END_DATE = datetime.now() - timedelta(days=1)
 
 
 def ee_init() -> None:
@@ -68,81 +71,6 @@ def ee_init() -> None:
         project=project,
         opt_url="https://earthengine-highvolume.googleapis.com",
     )
-
-
-def sample_points(date: datetime, bounds: Bounds, num_points: int) -> Iterable[Point]:
-    def get_coordinates(point: ee.Feature) -> ee.Feature:
-        coords = point.geometry().coordinates()
-        return ee.Feature(None, {"lat": coords.get(1), "lon": coords.get(0)})
-
-    ee_init()
-    image = get_image(LABELS, date).int()
-    points = image.stratifiedSample(
-        num_points,
-        region=ee.Geometry.Rectangle(bounds),
-        scale=SCALE,
-        geometries=True,
-    )
-    url = points.map(get_coordinates).getDownloadURL("CSV", ["lat", "lon"])
-    df = pd.read_csv(io.BytesIO(ee_fetch(url)))
-    for point in df.to_dict(orient="records"):
-        yield Point(date, point["lat"], point["lon"])
-
-
-def get_training_example(point: Point, patch_size: int) -> Tuple[Point, Example]:
-    ee_init()
-    inputs = get_input_data(point, patch_size)
-    labels = get_label_data(point, patch_size)
-    return (point, Example(inputs, labels))
-
-
-def get_input_data(point: Point, patch_size: int) -> np.ndarray:
-    return get_patch_sequence(point, patch_size, INPUT_HOUR_DELTAS, INPUTS, SCALE)
-
-
-def get_label_data(point: Point, patch_size: int) -> np.ndarray:
-    return get_patch_sequence(point, patch_size, LABEL_HOUR_DELTAS, LABELS, SCALE)
-
-
-def get_image(bands: Dict[str, List[str]], date: datetime) -> ee.Image:
-    end_date = date + IMAGE_TIME_WINDOW
-    images = [
-        ee.ImageCollection(collection)
-        .filterDate(date.isoformat(), end_date.isoformat())
-        .select(band_names)
-        .mosaic()
-        for collection, band_names in bands.items()
-    ]
-    return ee.Image(images)
-
-
-def get_patch(
-    image: ee.Image, lat: float, lon: float, patch_size: int, scale: int
-) -> np.ndarray:
-    point = ee.Geometry.Point([lon, lat])
-    url = image.getDownloadURL(
-        {
-            "region": point.buffer(scale * patch_size / 2, 1).bounds(1),
-            "dimensions": [patch_size, patch_size],
-            "format": "NPY",
-        }
-    )
-    data = np.load(io.BytesIO(ee_fetch(url)), allow_pickle=True)
-    return np.array([data[field] for field in data.dtype.names], dtype=np.float32)
-
-
-def get_patch_sequence(
-    p: Point,
-    patch_size: int,
-    hour_deltas: List[int],
-    bands: Dict[str, List[str]],
-    scale: int,
-) -> np.ndarray:
-    # TODO: batch sequence into a single ee.Image
-    dates = [p.date + timedelta(hours=h) for h in hour_deltas]
-    images = [get_image(bands, date) for date in dates]
-    patches = [get_patch(image, p.lat, p.lon, patch_size, scale) for image in images]
-    return np.stack(patches, axis=1)
 
 
 @retry.Retry(deadline=60 * 20)  # seconds
@@ -158,17 +86,84 @@ def ee_fetch(url: str) -> bytes:
     return response.content
 
 
-def random_date(start_date: datetime, end_date: datetime) -> datetime:
-    return start_date + (end_date - start_date) * random.random()
+def get_image(
+    date: datetime, bands_schema: Dict[str, List[str]], window: timedelta
+) -> ee.Image:
+    ee_init()
+    images = [
+        ee.ImageCollection(collection)
+        .filterDate(date.isoformat(), (date + window).isoformat())
+        .select(bands)
+        .mosaic()
+        for collection, bands in bands_schema.items()
+    ]
+    return ee.Image(images)
 
 
-def write_npz_file(point: Point, example: Example, file_prefix: str = "data") -> str:
+def sample_labels(
+    date: datetime, num_points: int, bounds: Bounds
+) -> Iterable[Tuple[datetime, Point]]:
+    image = get_image(date, LABELS, WINDOW)
+    for point in sample_points(image, num_points, bounds, SCALE):
+        yield (date, point)
+
+
+def sample_points(
+    image: ee.Image, num_points: int, bounds: Bounds, scale: int
+) -> Iterable[Point]:
+    def get_coordinates(point: ee.Feature) -> ee.Feature:
+        coords = point.geometry().coordinates()
+        return ee.Feature(None, {"lat": coords.get(1), "lon": coords.get(0)})
+
+    points = image.int().stratifiedSample(
+        num_points,
+        region=ee.Geometry.Rectangle(bounds),
+        scale=scale,
+        geometries=True,
+    )
+    url = points.map(get_coordinates).getDownloadURL("CSV", ["lat", "lon"])
+    df = pd.read_csv(io.BytesIO(ee_fetch(url)))
+    for point in df.to_dict(orient="records"):
+        yield Point(point["lat"], point["lon"])
+
+
+def get_training_example(date: datetime, point: Point, patch_size: int = 64) -> Example:
+    def patch_sequence(schema: Dict[str, List[str]], hours: List[int]) -> np.ndarray:
+        dates = [date + timedelta(hours=h) for h in hours]
+        images = [get_image(d, schema, WINDOW) for d in dates]
+        return get_patch_sequence(images, point, patch_size, SCALE)
+
+    ee_init()
+    return Example(
+        patch_sequence(INPUTS, [-2, -1, 0]),
+        patch_sequence(LABELS, [1, 2]),
+    )
+
+
+def get_patch_sequence(
+    image_sequence: List[ee.Image], point: Point, patch_size: int, scale: int
+) -> np.ndarray:
+    def unpack(arr: np.ndarray, i: int) -> np.ndarray:
+        names = [x for x in arr.dtype.names if x.startswith(f"{i}_")]
+        return np.moveaxis(structured_to_unstructured(arr[names]), -1, 0)
+
+    point = ee.Geometry.Point([point.lon, point.lat])
+    image = ee.ImageCollection(image_sequence).toBands()
+    url = image.getDownloadURL(
+        {
+            "region": point.buffer(scale * patch_size / 2, 1).bounds(1),
+            "dimensions": [patch_size, patch_size],
+            "format": "NPY",
+        }
+    )
+    flat_seq = np.load(io.BytesIO(ee_fetch(url)), allow_pickle=True)
+    return np.stack([unpack(flat_seq, i) for i, _ in enumerate(image_sequence)], axis=1)
+
+
+def write_npz_file(example: Example, file_prefix: str) -> str:
     from apache_beam.io.filesystems import FileSystems
 
-    date_id = str(point.date.date())
-    time_id = str(point.date.time()).replace(":", "-")
-    coords_id = f"{point.lat:.2f}@{point.lon:.2f}"
-    filename = FileSystems.join(file_prefix, date_id, time_id, coords_id + ".npz")
+    filename = FileSystems.join(file_prefix, f"{uuid.uuid4()}.npz")
     with FileSystems.create(filename) as f:
         np.savez_compressed(f, inputs=example.inputs, labels=example.labels)
     return filename
@@ -177,7 +172,7 @@ def write_npz_file(point: Point, example: Example, file_prefix: str = "data") ->
 def run(
     output_path: str,
     num_dates: int,
-    points_per_date: int,
+    num_points: int,
     bounds: Bounds,
     patch_size: int,
     beam_args: Optional[List[str]] = None,
@@ -185,8 +180,9 @@ def run(
     import apache_beam as beam
     from apache_beam.options.pipeline_options import PipelineOptions
 
-    start_date = datetime(2017, 7, 10)
-    end_date = datetime.now() - timedelta(days=1)
+    random_dates = [
+        START_DATE + (END_DATE - START_DATE) * random.random() for _ in range(num_dates)
+    ]
 
     beam_options = PipelineOptions(
         beam_args,
@@ -196,14 +192,11 @@ def run(
     with beam.Pipeline(options=beam_options) as pipeline:
         (
             pipeline
-            | "Create date range" >> beam.Create([(start_date, end_date)])
-            | "Repeat range" >> beam.FlatMap(lambda x: [x] * num_dates)
-            | "Random dates" >> beam.MapTuple(random_date)
-            | "Reshuffle dates" >> beam.Reshuffle()
-            | "Sample points" >> beam.FlatMap(sample_points, bounds, points_per_date)
-            | "Reshuffle points" >> beam.Reshuffle()
-            | "Get example" >> beam.Map(get_training_example, patch_size)
-            | "Write NPZ files" >> beam.MapTuple(write_npz_file, output_path)
+            | "Random dates" >> beam.Create(random_dates)
+            | "Sample labels" >> beam.FlatMap(sample_labels, num_points, bounds)
+            | "Reshuffle" >> beam.Reshuffle()
+            | "Get example" >> beam.MapTuple(get_training_example, patch_size)
+            | "Write NPZ files" >> beam.Map(write_npz_file, output_path)
             | "Log files" >> beam.Map(logging.info)
         )
 
@@ -216,7 +209,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output-path", required=True)
     parser.add_argument("--num-dates", type=int, default=100)
-    parser.add_argument("--points-per-date", type=int, default=100)
+    parser.add_argument("--num-points", type=int, default=100)
     parser.add_argument("--west", type=float, default=-125.3)
     parser.add_argument("--south", type=float, default=27.4)
     parser.add_argument("--east", type=float, default=-66.5)
@@ -227,7 +220,7 @@ if __name__ == "__main__":
     run(
         output_path=args.output_path,
         num_dates=args.num_dates,
-        points_per_date=args.points_per_date,
+        num_points=args.num_points,
         bounds=Bounds(args.west, args.south, args.east, args.north),
         patch_size=args.patch_size,
         beam_args=beam_args,
