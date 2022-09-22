f91910b7c19fc2a3302dc2c86df9eb1d9d9a82d5
David Cavazos <dcavazos@google.com>
2022-09-22 11:59:24 -0700

    modularize conftest

--- people-and-planet-ai/weather-forecasting/e2e_test.py
+++ people-and-planet-ai/weather-forecasting/e2e_test.py
@@ -12,26 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-import os
-import platform
-import re
-import subprocess
-from typing import Dict
-import uuid
-
-from google.cloud import storage
-from nbclient import NotebookClient
-import nbformat
+import conftest
 import pytest
 
-PYTHON_VERSION = "".join(platform.python_version_tuple()[0:2])
 
-NAME = f"ppai/weather-forecasting-py{PYTHON_VERSION}"
+@pytest.fixture(scope="session")
+def test_name(python_version: str) -> str:
+    return f"ppai/weather-forecasting-py{python_version}"
 
-UUID = uuid.uuid4().hex[0:6]
-PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
-LOCATION = "us-central1"
 
 PRELUDE = """
 from unittest.mock import Mock
@@ -42,23 +30,11 @@ exit = Mock()
 """
 
 
-@pytest.fixture(scope="session")
-def bucket_name() -> str:
-    storage_client = storage.Client()
-
-    bucket_name = f"{NAME.replace('/', '-')}-{UUID}"
-    bucket = storage_client.create_bucket(bucket_name, location=LOCATION)
-
-    logging.info(f"bucket_name: {bucket_name}")
-    yield bucket_name
-
-    subprocess.check_call(["gsutil", "-m", "rm", "-rf", f"gs://{bucket_name}/*"])
-    bucket.delete(force=True)
-
-
-def test_notebook(bucket_name: str) -> None:
+def test_notebook(
+    project: str, bucket_name: str, location: str, unique_name: str
+) -> None:
     # Create the datasets since this step is disabled from testing the notebook.
-    run_cmd(
+    conftest.run_cmd(
         [
             "python",
             "datasets.py",
@@ -66,78 +42,19 @@ def test_notebook(bucket_name: str) -> None:
             "--num-dates=1",
             "--num-points=1",
             "--runner=DataflowRunner",
-            f"--project={PROJECT}",
-            f"--region={LOCATION}",
+            f"--project={project}",
+            f"--region={location}",
             f"--temp_location=gs://{bucket_name}/temp",
             # Parameters for testing only, not used in the notebook.
-            f"--job_name={NAME.replace('/', '-')}-{UUID}",
+            f"--job_name={unique_name}",  # Dataflow job name
         ]
     )
 
     substitutions = {
-        "project": PROJECT,
+        "project": project,
         "bucket": bucket_name,
-        "location": LOCATION,
-        "display_name": f"{NAME.replace('/', '-')}-{UUID}",  # Vertex AI job
+        "location": location,
+        "display_name": unique_name,  # Vertex AI job name
         "epochs": 1,
     }
-    run_notebook("README.ipynb", substitutions, PRELUDE)
-
-
-# TODO: move these to a conftest.py
-
-
-def run_cmd(*cmd: str) -> subprocess.CompletedProcess:
-    try:
-        print(f">> {cmd}")
-        p = subprocess.run(
-            cmd,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        print(p.stdout.decode("utf-8"))
-        return p
-    except subprocess.CalledProcessError as e:
-        # Include the error message from the failed command.
-        print(e.stdout.decode("utf-8"))
-        raise Exception(f"{e}\n\n{e.stderr.decode('utf-8')}") from e
-
-
-def run_notebook(
-    ipynb_file: str,
-    substitutions: Dict[str, str] = {},
-    prelude: str = "",
-    remove_shell_commands: bool = True,
-) -> None:
-    # Regular expression to match and remove shell commands from the notebook.
-    #   https://regex101.com/r/auHETK/1
-    shell_command_re = re.compile(r"^!(?:[^\n]+\\\n)*(?:[^\n]+)$", re.MULTILINE)
-
-    # Compile regular expressions for variable substitutions.
-    #   https://regex101.com/r/AS7pDq/1
-    compiled_substitutions = [
-        (re.compile(rf"{name}\s*=\s*.+(?<!,)"), f"{name} = {value}")
-        for name, value in substitutions.items()
-    ]
-
-    nb = nbformat.read(ipynb_file, as_version=4)
-    for cell in nb.cells:
-        # Only preprocess code cells.
-        if cell["cell_type"] != "code":
-            continue
-
-        # Remove shell commands.
-        if remove_shell_commands:
-            cell["source"] = shell_command_re.sub("", cell["source"])
-
-        # Apply variable substitutions.
-        for regex, new_value in compiled_substitutions:
-            cell["source"] = regex.sub(new_value, cell["source"])
-
-    # Prepend the prelude cell.
-    nb.cells = [nbformat.v4.new_code_cell(prelude)] + nb.cells
-
-    # Run the notebook.
-    client = NotebookClient(nb)
-    client.execute()
+    conftest.run_notebook("README.ipynb", substitutions, PRELUDE)
