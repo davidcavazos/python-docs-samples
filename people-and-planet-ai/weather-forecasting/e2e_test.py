92a1c0f49a490d93d4b87c522841fb06b36c7f5b
David Cavazos <dcavazos@google.com>
2022-11-28 12:29:21 -0800

    add more tests

--- people-and-planet-ai/weather-forecasting/e2e_test.py
+++ people-and-planet-ai/weather-forecasting/e2e_test.py
@@ -116,11 +116,11 @@ def test_weather_forecasting_notebook(
         ),
         sections={
             "# 📚 Understand the data": {},
-            # "# 🗄 Create the dataset": {},
+            "# 🗄 Create the dataset": {},
             # "# ☁️ Create the dataset in Dataflow": {
             #     "replace": {'--runner="DataflowRunner"': dataflow_dataset_flags},
             # },
-            # "# 🧠 Train the model": {"variables": {"data_path": data_path}},
+            "# 🧠 Train the model": {"variables": {"data_path": data_path}},
             # "# ☁️ Train the model in Vertex AI": {},
             "# 🔮 Make predictions": {"variables": {"model_path": model_path}},
         },
